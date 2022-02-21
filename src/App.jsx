9bff1e6989319a1b6064ec7a5bf772bf9aec880f
007tickooayush <ayush.tickoo292000@gmail.com>
2022-02-21 15:00:22 +0530

    created public and private route

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import { Switch,Route } from 'react-router-dom';
+import { Switch } from 'react-router';
 import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 
 import './styles/main.scss';
 
-
 function App() {
   return (
     <Switch>
-      <Route >
-        <SignIn path='/signin'/>
-      </Route>
+      <PublicRoute path="/signin" >
+        <SignIn />
+      </PublicRoute>
 
-    <PrivateRoute>
-      
-    </PrivateRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
     </Switch>
   );
 }
