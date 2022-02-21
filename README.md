b7a4d66ed1e60c369dbc651017b10c1308662575
007tickooayush <ayush.tickoo292000@gmail.com>
2022-02-21 13:01:25 +0530

    initialized firebase project

--- README.md
+++ README.md
@@ -3,10 +3,29 @@
 ```
 npm install -D rimraf prettier babel-eslint eslint-plugin-react eslint-plugin-jsx-a11y eslint-plugin-import eslint-config-prettier eslint-config-airbnb eslint-plugin-react-hooks
 ```
-```
-npm install rsuite react-router react-router-dom
-```
-## : `install node sass version 4 =>`
+
+## : `To NOT have any discrepancies, please use  =>`
+## : `install node sass version 4, RSuite v4, React Router DOM, React Router v4 and Firebase v8 =>`
 ```
 npm i node-sass@4.14.1
+```
+```
+npm install rsuite@4.10.2
+```
+```
+npm install react-router-dom@5.2.0 react-router@5.2.0
+```
+```
+npm install -g firebase-tools
+npm install firebase@8.10.0
+```
+
+## : `to initialize firebase =>`
+*`for login: `*
+```
+firebase login
+```
+*`for initialization: `*
+```
+firebase init
 ```
---
+++ src/misc/firebase.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDT0hnoLxmMXoHU4CVDX0Dps2AW-2QYrb0",
+  authDomain: "chat-web-app-6d201.firebaseapp.com",
+  projectId: "chat-web-app-6d201",
+  storageBucket: "chat-web-app-6d201.appspot.com",
+  messagingSenderId: "80384873505",
+  appId: "1:80384873505:web:3610a2f26a30bce24c3f60"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// // Import the functions you need from the SDKs you need
+// import {firebase, initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDT0hnoLxmMXoHU4CVDX0Dps2AW-2QYrb0",
+//   authDomain: "chat-web-app-6d201.firebaseapp.com",
+//   projectId: "chat-web-app-6d201",
+//   storageBucket: "chat-web-app-6d201.appspot.com",
+//   messagingSenderId: "80384873505",
+//   appId: "1:80384873505:web:3610a2f26a30bce24c3f60"
+// };
+
+// const app = firebase.initializeApp(firebaseConfig);
