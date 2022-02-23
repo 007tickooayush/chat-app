2dabd4520e97c67b00b8250615911adfe5981684
007tickooayush <ayush.tickoo292000@gmail.com>
2022-02-23 14:29:26 +0530

    set up updation nick name inside the database

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Drawer, Button,Divider} from 'rsuite';
+import { Drawer, Button, Divider, Alert } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import { database } from '../../misc/firebase';
 import EditableInput from '../EditableInput';
 
-const Dashboard = ({onSignOut}) => {
-  const {profile} = useProfile();
+const Dashboard = ({ onSignOut }) => {
+  const { profile } = useProfile();
 
   // console.log('profile', profile);
 
-  const onSave = async (newData) =>{
-    // log newData
-    console.log('newData', newData);
+  const onSave = async newData => {
+    // log newDataq
+    // console.log('newData', newData);
+
+    const usernameNickNameRef = database
+      .ref(`/profiles/${profile.uid}`)
+      .child('name');
+    try {
+      await usernameNickNameRef.set(newData);
+
+      Alert.info('Nickname is updated', 4300);
+    } catch (error) {
+      Alert.error(error.message, 4300);
+    }
   };
 
   return (
@@ -21,13 +33,12 @@ const Dashboard = ({onSignOut}) => {
 
       <Drawer.Body>
         <h3> Hey, {profile.name}</h3>
-        <Divider/>
-        <EditableInput 
+        <Divider />
+        <EditableInput
           name="nickname"
           initialValue={profile.name}
           onSave={onSave}
-          label={<h6 className='mb-2'>Nickname</h6>}
-
+          label={<h6 className="mb-2">Nickname</h6>}
         />
       </Drawer.Body>
 
