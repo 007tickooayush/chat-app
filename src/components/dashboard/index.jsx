4de3f5ab4ff9cd84eea34004cda1180ceda4a7fd
007tickooayush <ayush.tickoo292000@gmail.com>
2022-02-23 13:06:47 +0530

    created reusable/editable input for nickname field

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Drawer, Button } from 'rsuite';
+import { Drawer, Button,Divider} from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import EditableInput from '../EditableInput';
 
 const Dashboard = ({onSignOut}) => {
   const {profile} = useProfile();
 
   // console.log('profile', profile);
+
+  const onSave = async (newData) =>{
+    // log newData
+    console.log('newData', newData);
+  };
+
   return (
     <>
       <Drawer.Header>
@@ -14,6 +21,14 @@ const Dashboard = ({onSignOut}) => {
 
       <Drawer.Body>
         <h3> Hey, {profile.name}</h3>
+        <Divider/>
+        <EditableInput 
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className='mb-2'>Nickname</h6>}
+
+        />
       </Drawer.Body>
 
       <Drawer.Footer>
