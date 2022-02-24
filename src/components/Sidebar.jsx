1f1150c42fdf310a2fbc910e50d17531524f6d2c
007tickooayush <ayush.tickoo292000@gmail.com>
2022-02-24 12:45:56 +0530

    added functionality for scroolheight

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React,{useRef,useState,useEffect} from 'react';
+import { Divider } from 'rsuite';
 import CreateRoomBtnModal from './CreateRoomBtnModal';
 import DashboardToggle from './dashboard/DashboardToggle';
+import ChatRoomList from './rooms/ChatRoomList';
 
 const Sidebar = () => {
+
+  const topSidebarRef = useRef();
+  const [height, setHeight] = useState(null);
+
+  useEffect(() => {
+    if(topSidebarRef.current){
+      setHeight(topSidebarRef.current.scrollHeight)
+    }
+  }, [topSidebarRef]);
+  
+
   return (
     <div className="h-100 pt-2">
-      <div>
+      <div ref={topSidebarRef}>  
         <DashboardToggle />
         <CreateRoomBtnModal/>
+        <Divider>Join Conversation</Divider>
       </div>
 
-      <div>bottom</div>
+      <div>
+        <ChatRoomList aboveElHeight={height}/>
+      </div>
     </div>
   );
 };
