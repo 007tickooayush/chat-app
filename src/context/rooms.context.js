d9e9d92b4f04bdb0972a37f15f3aa5723723a59c
007tickooayush <ayush.tickoo292000@gmail.com>
2022-02-24 14:18:05 +0530

    added room list mangement for sidebar

--- src/context/rooms.context.js
+++ src/context/rooms.context.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState,useEffect } from 'react';
+import React, { useContext, createContext, useState, useEffect } from 'react';
 import { database } from '../misc/firebase';
 import { transformToArrWithId } from '../misc/helpers';
 
@@ -7,20 +7,22 @@ const RoomsContext = createContext();
 export const RoomsProvider = ({ children }) => {
   const [rooms, setRooms] = useState(null);
 
-  useEffect(()=>{
+  useEffect(() => {
     const roomListRef = database.ref('rooms');
-    roomListRef.on('value',(snap) =>{
-        const data = transformToArrWithId(snap.val()); 
-        // log snapshot data
-        // console.log('snap data', data);
-        setRooms(data);
-    })
+    roomListRef.on('value', snap => {
+      const data = transformToArrWithId(snap.val());
+      // log snapshot data
+      // console.log('snap data', data);
+      setRooms(data);
+    });
 
     return () => {
-        roomListRef.off();
-    }
-  },[])
+      roomListRef.off();
+    };
+  }, []);
   return (
     <RoomsContext.Provider value={rooms}>{children}</RoomsContext.Provider>
   );
 };
+
+export const useRooms = () => useContext(RoomsContext);
