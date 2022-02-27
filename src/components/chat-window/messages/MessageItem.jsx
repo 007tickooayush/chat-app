2daf4e766badb29aa444701978aaf7a496245204
007tickooayush <ayush.tickoo292000@gmail.com>
2022-02-27 11:09:13 +0530

    src/

---
+++ src/components/chat-window/messages/MessageItem.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import TimeAgo from 'timeago-react';
+import ProfileAvatar from '../../ProfileAvatar';
+
+const MessageItem = ({ message }) => {
+  const { author, createdAt, text } = message;
+
+  return (
+    <ul className="padded mb-1">
+      <div className="d-flex align-items-center font-bolder">
+        <ProfileAvatar
+          src={author.avatar}
+          name={author.name}
+          className="ml-1"
+          size="xs"
+        />
+        <span className="ml-2"> {author.name}</span>
+        <TimeAgo
+          datetime={
+            createdAt
+          }
+          className="font-normal text-black-45 ml-2"
+        />
+      </div>
+
+      <div className="">
+        <span className="word-break-all ">{text}</span>
+      </div>
+    </ul>
+  );
+};
+
+export default MessageItem;
