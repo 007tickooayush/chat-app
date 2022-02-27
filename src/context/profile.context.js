794d3269a97bc2ecf0a189eb781e6f69e82bfb98
007tickooayush <ayush.tickoo292000@gmail.com>
2022-02-27 19:31:02 +0530

    realtime prescence listener for user added

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,66 +1,103 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
 import React, { createContext, useState, useEffect, useContext } from 'react';
+import firebase from 'firebase/app';
 import { auth, database } from '../misc/firebase';
+
+export const isOfflineForDatabase = {
+  state: 'offline',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
+const isOnlineForDatabase = {
+  state: 'online',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
 
 // creating context API to avoid 'prop drilling'
 const ProfileContext = createContext();
 
 export const ProfileProvider = ({ children }) => {
-    const [profile, setProfile] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
+  const [profile, setProfile] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        let userRef;
+  useEffect(() => {
+    let userRef;
+    let userStatusRef;
 
-        const authOnSub = auth.onAuthStateChanged(authObj => {
-            //   console.log('authObj',authObj)
+    const authOnSub = auth.onAuthStateChanged(authObj => {
+      //   console.log('authObj',authObj)
 
-            if (authObj) {
-                userRef = database.ref(`/profiles/${authObj.uid}`);
+      if (authObj) {
+        userStatusRef = database.ref(`/status/${authObj.uid}`);
+        userRef = database.ref(`/profiles/${authObj.uid}`);
 
-                userRef.on('value', snapshot => {
-                    // log entire snapshot
-                    // console.log('snapshot', snapshot);
+        userRef.on('value', snapshot => {
+          // log entire snapshot
+          // console.log('snapshot', snapshot);
 
-                    const { name, createdAt,avatar } = snapshot.val();
-                    
-                    const profileData = {
-                        name,
-                        createdAt,
-                        avatar,
-                        uid: authObj.uid,
-                        email: authObj.email,
-                    };
-                    // log profileData
-                    // console.log('profileData', profileData);
+          const { name, createdAt, avatar } = snapshot.val();
 
-                    setProfile(profileData);
-                    setIsLoading(false);
-                });
-            } else {
-                if(userRef){
-                    userRef.off();
-                }
+          const profileData = {
+            name,
+            createdAt,
+            avatar,
+            uid: authObj.uid,
+            email: authObj.email,
+          };
+          // log profileData
+          // console.log('profileData', profileData);
 
-                setProfile(null);
-                setIsLoading(false);
-            }
+          setProfile(profileData);
+          setIsLoading(false);
         });
-        return () => {
-            authOnSub();
 
-            // unsubsrcibe from user reference
-            if(userRef){
-                userRef.off();
-            }
-        };
-    }, []);
+        database.ref('.info/connected').on('value', snapshot => {
+          if (snapshot.val() === false) {
+            return;
+          }
+          userStatusRef
+            .onDisconnect()
+            .set(isOfflineForDatabase)
+            .then(() => {
+              userStatusRef.set(isOnlineForDatabase);
+            });
+        });
+      } else {
+        if (userRef) {
+          userRef.off();
+        }
+        if (userStatusRef) {
+          userStatusRef.off();
+        }
 
-    return (
-        <ProfileContext.Provider value={{ profile, isLoading }}>
-            {children}
-        </ProfileContext.Provider>
-    );
+        database.ref('.info/connected').off();
+
+        setProfile(null);
+        setIsLoading(false);
+      }
+    });
+    return () => {
+      authOnSub();
+
+      // unsubsrcibe from user reference
+      if (userRef) {
+        userRef.off();
+      }
+
+      if (userStatusRef) {
+        userStatusRef.off();
+      }
+
+      database.ref('.info/connected').off();
+
+    };
+  }, []);
+
+  return (
+    <ProfileContext.Provider value={{ profile, isLoading }}>
+      {children}
+    </ProfileContext.Provider>
+  );
 };
 
 export const useProfile = () => useContext(ProfileContext);
