88709402181820380649a967f0dfd26afcf21287
007tickooayush <ayush.tickoo292000@gmail.com>
2022-02-28 09:04:31 +0530

    added room edit drawer to chat/top

--- src/components/chat-window/top/index.jsx
+++ src/components/chat-window/top/index.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { ButtonToolbar, Icon } from 'rsuite';
 import { useCurrentRoom } from '../../../context/current-room.context';
 import { useMediaQuery } from '../../../misc/custom-hooks';
+import EditRoomBtnDrawer from './EditRoomBtnDrawer';
 import RoomInfoBtnModal from './RoomInfoBtnModal';
 
 const Top = () => {
@@ -13,7 +14,7 @@ const Top = () => {
   return (
     <div>
       <div className="d-flex justify-content-between align-items-center">
-        <h4 className='text-disappear d-flex align-items-center'>
+        <h4 className="text-disappear d-flex align-items-center">
           <Icon
             componentClass={Link}
             to="/"
@@ -28,12 +29,14 @@ const Top = () => {
           <span className="text-disappear">{name}</span>
         </h4>
 
-        <ButtonToolbar className='ws-nowrap' >todo</ButtonToolbar>
+        <ButtonToolbar className="ws-nowrap">
+          <EditRoomBtnDrawer />
+        </ButtonToolbar>
       </div>
 
-      <div className='d-flex justify-content-between align-items-center'>
+      <div className="d-flex justify-content-between align-items-center">
         <span>todo</span>
-        <RoomInfoBtnModal/>
+        <RoomInfoBtnModal />
       </div>
     </div>
   );
